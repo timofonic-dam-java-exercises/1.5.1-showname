b40dc717574e421581cd11ed56e77a3d736f0e01
timofonic <timofonic@gmail.com>
2017-10-30 11:06:45 +0100

    Adding address and phone

--- ShowName.java
+++ ShowName.java
@@ -1,14 +1,18 @@
-public class ShowName {
+public class ShowNameAddressPhone {
 
     public static void main(String[] args) {
 
-        String[] name = {"Tim", "Ofo", "Nic"}; // declares an array of 
-        strings
+        String[] name = {"Tim", "Ofo", "Nic", "428 Elm Street. Springwood, Ohio","555-9046"}; // declares an array of strings
         System.out.println("My First name is: "
                 + name[0]);
         System.out.println("My First Last Name is: "
                 + name[1]);
         System.out.println("My Second Last Name is: "
                 + name[2]);
+        System.out.println("My address: "
+                + name[3]);
+        System.out.println("Phone: "
+                + name[4]);
+                                        
     }
 }
