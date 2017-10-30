27b991d34f04d488627f1ccec1709efdcc8372e0
timofonic <timofonic@gmail.com>
2017-10-30 11:03:09 +0100

    Commit simple program showing name, using a very simple array structure

---
+++ ShowName.java
@@ -0,0 +1,14 @@
+public class ShowName {
+
+    public static void main(String[] args) {
+
+        String[] name = {"Tim", "Ofo", "Nic"}; // declares an array of 
+        strings
+        System.out.println("My First name is: "
+                + name[0]);
+        System.out.println("My First Last Name is: "
+                + name[1]);
+        System.out.println("My Second Last Name is: "
+                + name[2]);
+    }
+}
